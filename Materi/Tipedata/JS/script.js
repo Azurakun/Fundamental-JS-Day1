83d95e35c40283e5743b9fbc55eeda327c9ebcee
AzuraKun <wizardcool555@gmail.com>
2024-01-25 15:27:38 +0700

    Pengkondisian

---
+++ Materi/Tipedata/JS/script.js
@@ -0,0 +1,29 @@
+// console.log(1, typeof 1);
+// console.log(1, typeof "1");
+// console.log(true, typeof true);
+
+// const human = {
+//     'name' : 'Azura',
+//     'Age' : '18'
+// }
+
+// const humanArray = [
+//     {
+//         name : 'isma',
+//         age  : 12
+//     },
+//     {
+//         name : 'lila',
+//         age  : 18
+//     }
+// ]
+
+// console.log(typeof human);
+// console.log(typeof humanArray);
+
+var input = prompt("input data :")
+if (!isNaN(input)) {
+    console.log("Tipe data: Integer")
+} else {
+    console.log("tipe data: String")
+}
